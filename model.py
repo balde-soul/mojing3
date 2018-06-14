8953da5aabef0570430a76efd0ab4fbdb4992a6d
sins <CJHdream@outlook.com>
2018-06-14 12:10:38 +0800

    none:

---
+++ model.py
@@ -0,0 +1,27 @@
+# coding = utf-8
+
+import tensorflow as tf
+import tensorflow.contrib.rnn as rnn
+import pandas as pd
+
+input_len = 300
+output_len = 300
+
+def BILSTM(hidden_num, use_dropout):
+
+    def rnn():
+        pass
+    pass
+
+class Model:
+    def __init__(self):
+        pass
+
+    def build(self):
+        pass
+
+    def train(self):
+        pass
+
+    def eval(self):
+        pass
