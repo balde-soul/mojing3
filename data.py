237444bc40f846734b2510cc93439f17d9af131b
blade <CJHdream@outlook.com>
2018-06-16 21:34:12 +0800

    none

---
+++ data.py
@@ -0,0 +1,14 @@
+#coding = utf-8
+import pandas as pd
+
+word_inf = pd.read_csv('./data/question.csv')
+
+
+def extract_question(question):
+    print(word_inf['qid'])
+    word_inf.
+    pass
+
+extract_question(00)
+
+
